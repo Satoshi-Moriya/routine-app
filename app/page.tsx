595c37ee1b0722ef879222056aecddc8879c7ad9
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-10-27 18:42:25 +0900

    top見た目をある程度整えた

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,14 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import AccountCircle from '@mui/icons-material/AccountCircle';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Grid from '@mui/material/Grid';
+import ListItemText from '@mui/material/ListItemText';
+import Container from '@mui/material/Container';
+
+// dbからのデータ
+const routineItem = ['ドラム練習', '勉強'];
 
 export default function Home() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -26,62 +34,76 @@ export default function Home() {
 
   return (
     <>
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            ルーティーンアプリ
-          </Typography>
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>アカウント設定</MenuItem>
-                <MenuItem onClick={handleClose}>ログアウト</MenuItem>
-              </Menu>
-            </div>
-        </Toolbar>
-      </AppBar>
-    </Box>
-
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Container maxWidth="lg">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                ルーティーンアプリ
+              </Typography>
+                <div>
+                  <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleMenu}
+                    color="inherit"
+                  >
+                    <AccountCircle />
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                  >
+                    <MenuItem onClick={handleClose}>アカウント設定</MenuItem>
+                    <MenuItem onClick={handleClose}>ログアウト</MenuItem>
+                  </Menu>
+                </div>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </Box>
       <main className={styles.main}>
-        <h2>君のルーティーン一覧</h2>
-        <ul>
-          <li>
-            <p>ドラム練習</p><Button variant='contained'>編集</Button><Button variant='outlined' endIcon={<DeleteIcon/>}>削除</Button>
-          </li>
-          <li>
-            <p>勉強</p><Button variant='contained'>編集</Button><Button variant='outlined' endIcon={<DeleteIcon/>}>削除</Button>
-          </li>
-        </ul>
-        <button>
-          <p>新たにルーティーンを追加する</p>
-          <span>+</span>
-        </button>
+        <Container maxWidth="sm">
+          <h2 className={styles.title}>君のルーティーン一覧</h2>
+          <Grid item xs={12} md={6}>
+            <List>
+              {
+                routineItem.map((text, index) => {
+                  return (
+                    <ListItem key={index} className={styles.listItem}>
+                      <ListItemText
+                        primary={text}
+                      />
+                      <div className={styles.listItemButtons}>
+                        <Button variant='contained'>編集</Button>
+                        <Button variant='outlined' endIcon={<DeleteIcon/>}>削除</Button>
+                      </div>
+                    </ListItem>
+                  );
+                })
+              }
+            </List>
+          </Grid>
+          <button>
+            <p>新たにルーティーンを追加する</p>
+            <span>+</span>
+          </button>
+        </Container>
       </main>
-      <footer>
+      <footer className={styles.footer}>
         <small>© 2023 ルーティーンアプリ</small>
       </footer>
     </>
