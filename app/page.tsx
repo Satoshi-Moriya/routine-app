30cbdd7bb28bc959f5ba5b96869c71be72f419c1
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-10-30 21:09:49 +0900

    amplifyを導入

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,14 @@ import styles from './page.module.css'
 import Container from '@mui/material/Container';
 import Header from '@/components/Header';
 import Hooter from '@/components/Footer';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css'
 
-export default function Home() {
+import awsExports from '../src/aws-exports.js';
+Amplify.configure(awsExports);
+
+export default withAuthenticator(function Home({signOut, user}) {
 
   return (
     <>
@@ -14,10 +20,18 @@ export default function Home() {
       <main className={styles.main}>
         <Container maxWidth="sm">
           <h2 className={styles.title}>ログイン画面</h2>
+          {
+            user ? (
+              <>
+                <h3>私は権限を持っています:{user.username}</h3>
+                <button onClick={signOut}>サインアウト</button>
+              </>
+            ) : <h3>権限がありません</h3>
+          }
           <a href="/routine">ルーティーン一覧画面へ</a>
         </Container>
       </main>
       <Hooter />
     </>
   )
-}
+});
