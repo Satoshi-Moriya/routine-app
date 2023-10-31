8b07175cc7703ab1ab49fa9d9ef541b0c6aff86f
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-10-31 13:08:46 +0900

    ビルド失敗を修正2

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import { Amplify } from 'aws-amplify';
 import {Authenticator} from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css'
 
-import awsExports from '../lib/aws-exports.js';
+import awsExports from '../src/aws-exports.js';
 Amplify.configure(awsExports);
 
 export default function Home() {
